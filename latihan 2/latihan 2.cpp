f3a8df4cc0099947c2d790b4ecba003e45159d10
PutriHabibahDevina <putrihdm13@gmail.com>
2023-04-03 10:54:48 +0700

    int main

--- latihan 2/latihan 2.cpp
+++ latihan 2/latihan 2.cpp
@@ -15,5 +15,28 @@ struct Mahasiswa
 	int umur;
 };
 
+int main() {
+	Mahasiswa mhs;
+
+	cout << "Masukan NIM :";
+	cin >> mhs.NIM;
+	cout << "Masukan Nama :";
+	cin >> mhs.nama;
+	cout << "Alamat :";
+	cout << "\n\tMasukan Desa :";
+	cin >> mhs.alamat.desa;
+	cout << "\tMasukan Kota :";
+	cin >> mhs.alamat.kota;
+	cout << "Masukan Umur :";
+	cin >> mhs.umur;
+
+	cout << "\n\nMIM = " << mhs.NIM;
+	cout << "\nNama = " << mhs.nama;
+	cout << "\nDesa = " << mhs.alamat.desa;
+	cout << "\nKota = " << mhs.alamat.kota;
+	cout << "\nUmur = " << mhs.umur;
+}
+
+
 
 
