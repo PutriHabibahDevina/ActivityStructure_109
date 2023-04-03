ce1a781d17debf5d3923b6bc3518458fdc47c63a
PutriHabibahDevina <putrihdm13@gmail.com>
2023-04-03 10:30:52 +0700

    int main

--- ActivityStructure_109/ActivityStructure_109.cpp
+++ ActivityStructure_109/ActivityStructure_109.cpp
@@ -9,4 +9,31 @@ struct mahasiswa
 	int umur;
 };
 
+int main() {
+	mahasiswa mhs1, mhs2;
+
+	mhs1.NIM = "20220140109";
+	mhs1.nama = "Putri Habibah";
+	mhs1.alamat = "Yogyakarta";
+	mhs1.umur = 20;
+
+	cout << "Masukan NIM :";
+	cin >> mhs2.NIM;
+	cout << "Masukan Nama :";
+	cin >> mhs2.nama;
+	cout << "Masukan Alamat :";
+	cin >> mhs2.alamat;
+	cout << "Masukan Umur :";
+	cin >> mhs2.umur;
+
+	cout << "\nNIM = " << mhs1.NIM;
+	cout << "\nNama = " << mhs1.nama;
+	cout << "\nAlamat = " << mhs1.alamat;
+	cout << "\nUmur  = " << mhs1.umur;
+
+	cout << "\nNIM = " << mhs2.NIM;
+	cout << "\nNama = " << mhs2.nama;
+	cout << "\nAlamat = " << mhs2.alamat;
+	cout << "\nUmur = " << mhs2.umur;
+}
 
